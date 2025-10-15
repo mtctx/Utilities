97b0e5c5977173eeb2d6edef1ec8f571056c26f6
mtctx <me@mtctx.dev>
2025-10-15 15:36:32 +0200

    Added Outcome & License

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,104 @@
+/*
+ *     Utilities: build.gradle.kts
+ *     Copyright (C) 2025 mtctx
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "2.2.0"
+    id("com.vanniktech.maven.publish") version "0.34.0"
+    id("org.jetbrains.dokka") version "2.1.0-Beta"
+    id("org.jetbrains.dokka-javadoc") version "2.1.0-Beta"
+    signing
 }
 
 group = "dev.mtctx.library"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.1.0-Beta")
 }
 
-tasks.test {
-    useJUnitPlatform()
+mavenPublishing {
+    coordinates(group.toString(), "utilities", version.toString())
+
+    pom {
+        name.set("mtctx utilities")
+        description.set("Things I often use in my software, e.g. Outcome")
+        inceptionYear.set("2025")
+        url.set("https://github.com/mtctx/Utils")
+
+        licenses {
+            license {
+                name.set("GNU General Public License v3.0")
+                url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                distribution.set("repo")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/mtctx/Utils")
+            connection.set("scm:git:git@github.com:mtctx/Utils.git")
+            developerConnection.set("scm:git:ssh://git@github.com:mtctx/Utils.git")
+        }
+
+        developers {
+            developer {
+                id.set("mtctx")
+                name.set("mtctx")
+                email.set("me@mtctx.dev")
+            }
+        }
+    }
+
+    configure(KotlinJvm(JavadocJar.Dokka("dokkaGenerateJavadoc"), sourcesJar = true))
+
+    signAllPublications()
+    publishToMavenCentral(automaticRelease = true)
 }
+
+signing {
+    useGpgCmd()
+}
+
+dokka {
+    dokkaPublications.html {
+        outputDirectory.set(layout.buildDirectory.dir("dokka/html").get().asFile)
+    }
+    dokkaPublications.javadoc {
+        outputDirectory.set(layout.buildDirectory.dir("dokka/javadoc").get().asFile)
+    }
+
+    dokkaSourceSets.configureEach {
+        jdkVersion.set(21)
+        sourceLink {
+            localDirectory.set(file("${project.name}/src/main/kotlin"))
+            remoteUrl.set(uri("https://github.com/mtctx/Squishy/tree/main/${project.name}/src/main/kotlin/"))
+            remoteLineSuffix.set("#L")
+        }
+    }
+}
+
+val javaTarget = 21
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(javaTarget)
 }
--- src/main/kotlin/mtctx/utilities/Outcome.kt
+++ src/main/kotlin/mtctx/utilities/Outcome.kt
@@ -18,5 +18,124 @@
 
 package mtctx.utilities
 
-class Outcome {
+import mtctx.utilities.Outcome.Failure
+import mtctx.utilities.Outcome.Success
+
+sealed interface Outcome<out T> {
+    val succeeded: Boolean
+    val failed: Boolean get() = !succeeded
+
+    @JvmInline
+    value class Success<T>(val value: T) : Outcome<T> {
+        override val succeeded: Boolean get() = true
+        override fun toString(): String = "Success(value=$value, type=${value!!.javaClass.name})"
+    }
+
+    data class Failure(val message: String, val throwable: Throwable? = null, val outcome: Outcome<*>? = null) :
+        Outcome<Nothing> {
+        override val succeeded: Boolean get() = false
+        override fun toString(): String =
+            "Failure(message=$message" + if (throwable != null) ", cause=${throwable.javaClass.name})" else ")"
+
+        fun toStringWithStackTrace(): String =
+            "Failure(message=$message" + if (throwable != null) ", stacktrace=${throwable.stackTraceToString()})" else ")"
+    }
 }
+
+inline fun <T, R> Outcome<T>.map(transform: (T) -> R): Outcome<R> = when (this) {
+    is Success -> Success(transform(value))
+    is Failure -> this
+}
+
+inline fun <T, R> Outcome<T>.flatMap(transform: (T) -> Outcome<R>): Outcome<R> = when (this) {
+    is Success -> transform(value)
+    is Failure -> this
+}
+
+inline fun <T, R> Outcome<T>.mapCatching(transform: (T) -> R): Outcome<R> = when (this) {
+    is Success ->
+        try {
+            Success(transform(value))
+        } catch (t: Throwable) {
+            Failure("Exception in mapCatching", t)
+        }
+
+    is Failure -> this
+}
+
+inline fun <T> Outcome<T>.mapFailure(transform: (Failure) -> Failure): Outcome<T> = when (this) {
+    is Success -> this
+    is Failure -> transform(this)
+}
+
+inline fun <T, R> Outcome<T>.fold(onSuccess: (T) -> R, onFailure: (String, Throwable?) -> R): R = when (this) {
+    is Success -> onSuccess(value)
+    is Failure -> onFailure(message, throwable)
+}
+
+inline fun <T> Outcome<T>.recover(recoverBlock: (Failure) -> T): Outcome<T> = when (this) {
+    is Success -> this
+    is Failure -> success(recoverBlock(this))
+}
+
+inline fun <T> Outcome<T>.recoverCatching(recoverBlock: (Failure) -> T): Outcome<T> = when (this) {
+    is Success -> this
+    is Failure ->
+        try {
+            success(recoverBlock(this))
+        } catch (t: Throwable) {
+            failure("Exception in recoverCatching", t)
+        }
+}
+
+fun <T> Outcome<T>.getOrNull(): T? = when (this) {
+    is Success -> value
+    is Failure -> null
+}
+
+fun <T> Outcome<T>.exceptionOrNull(): Throwable? = when (this) {
+    is Success -> null
+    is Failure -> throwable
+}
+
+fun <T> Outcome<T>.toResult(): Result<T> = when (this) {
+    is Success -> Result.success(value)
+    is Failure -> Result.failure(throwable ?: Exception(message))
+}
+
+fun <T> Result<T>.toOutcome(): Outcome<T> = fold(::success, ::failure)
+
+inline fun <T> runCatchingOutcome(block: () -> T): Outcome<T> {
+    return try {
+        success(block())
+    } catch (e: Throwable) {
+        failure(e)
+    }
+}
+
+inline fun <T> runCatchingOutcome(block: () -> Outcome<T>): Outcome<T> {
+    return try {
+        block()
+    } catch (e: Throwable) {
+        failure(e)
+    }
+}
+
+inline fun <T> T.runCatchingOutcome(block: () -> T): Outcome<T> =
+    try {
+        success(block())
+    } catch (t: Throwable) {
+        failure("Exception in runCatching Outcome", t)
+    }
+
+fun success(): Success<Boolean> = Success(true)
+fun <T> success(value: T): Success<T> = Success(value)
+inline fun <T> success(block: () -> T): Success<T> {
+    return Success(block())
+}
+
+fun failure(message: String, throwable: Throwable? = null, outcome: Outcome<*>? = null): Failure =
+    Failure(message, throwable, outcome)
+
+fun failure(throwable: Throwable, outcome: Outcome<*>? = null): Failure = Failure("No message provided.", throwable)
+fun failure(outcome: Outcome<*>? = null): Failure = Failure("No message provided.", outcome = outcome)
